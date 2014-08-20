5fc85af9e6d5801ea2d4f32a452fcc69f7756f02
unknown <hanyanan@540765150A961F7.internal.baidu.com>
2014-08-20 18:44:09 +0800

    backup

---
+++ Storage/src/main/java/com/hanyanan/tools/storage/disk/DiskStorage.java
@@ -0,0 +1,71 @@
+package com.hanyanan.tools.storage.disk;
+
+import android.graphics.Bitmap;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Created by hanyanan on 2014/8/20.
+ * This interface define the major function for the disk storage, it provide the major operation
+ * for user to implement it.
+ */
+public interface DiskStorage {
+    /**
+     * return root directory fot this storage module.
+     * @return root directory
+     */
+    File getDirectory();
+
+    /**
+     * return current size has cost.May be it's different from size which get from Operation System.
+     * @return current file size under root directory.
+     */
+    long getCurrentSize();
+
+    /**
+     * return clean file of key. In fact it's not recommend use this method to get the expected file.
+     * @see #getInputStream(String).
+     * @param key key
+     * @return file of key, null means that missing the key
+     */
+    File get(String key);
+
+    /**
+     * Get the input stream of key, it's recommend to read data from storage. Notices that it need
+     * to close the stream.
+     * @param key key
+     * @return  a input stream of current key, null means that it's not exits..
+     */
+    InputStream getInputStream(String key);
+
+    /**
+     * Provide a safe and strong way(but not easy) to write data to storage, user can encoding data,
+     * But remember close stream, or it useless.
+     * @param key
+     * @return output stream of current key, null means that failed.
+     */
+    OutputStream getOutputStream(String key);
+    /**
+     * Save the current input stream to storage. But notice that it's unsafe, it storage content
+     * without encoding.
+     * @param key key
+     * @param inputStream the input stream need read and write to storage.
+     * @return true means success, or other wise means failed.
+     * @throws IOException
+     */
+    boolean save(String key, InputStream inputStream) throws IOException;
+
+    /**
+     * 
+     * @param key
+     * @param bitmap
+     * @param format
+     * @param quality
+     * @return
+     * @throws IOException
+     */
+    boolean save(String key, Bitmap bitmap, Bitmap.CompressFormat format, int quality) throws IOException;
+}
